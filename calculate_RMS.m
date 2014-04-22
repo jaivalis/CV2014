4cc82ea0d11728901eb6b67844b56c3452dc9c7e
spimicha <spimicha@gmail.com>
2014-04-22 16:58:42 +0200

    calculate_RMS
    calculate_RMS(idxs, base_coords, target_coords, neighbors)

---
+++ calculate_RMS.m
@@ -0,0 +1,9 @@
+function [rms] = calculate_RMS(idxs, base_coords, target_coords, neighbors)
+
+    for i = 1 : size(target_coords, 1)
+        nn = kdtree_k_nearest_neighbors(tree, target_coords(i,:), neighbors);
+        RMS = RMS + sqrt(base_coords(nn,:) * target_coords(i,:)');
+        idxs(i,:) = nn';
+    end
+    RMS = RMS / size(target_coords, 1);
+    rms = RMS;
