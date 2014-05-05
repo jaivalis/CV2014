c1c3fb31e364983889b423ca8268c254b962c505
TopSteely <tobias.stahl1@gmx.de>
2014-05-05 18:26:31 +0200

    sampling 8 points with nearest neighbors of centerOfMass

--- a2/A2.m
+++ a2/A2.m
@@ -1,28 +1,35 @@
-run('C:\Users\T\Downloads\vlfeat-0.9.14-bin\vlfeat-0.9.14\toolbox\vl_setup.m');
+clear all
+addpath('../a1/kdtree')
 i1 = single(rgb2gray(imread('TeddyBear/obj02_001.png')));
+%i1 = single(imread('House/frame00000001.png'));
 i2 = single(rgb2gray(imread('TeddyBear/obj02_002.png')));
+%i2 = single(imread('House/frame00000002.png'));
 [i_points1, d1] = vl_sift(i1);
 [i_points2, d2] = vl_sift(i2);
+size(i_points1)
 
-% sample interest points for non background points
+%sample interest points for non background points
 [i_points1, d1] = getForegroundSift(i_points1, d1);
 [i_points2, d2] = getForegroundSift(i_points2, d2);
+size(i_points1)
 
 % match the foreground points with eachother
 [matches, scores] = vl_ubcmatch(d1, d2) ; % extract and match the descriptors 
+size(matches)
+% % sample 8 points from matches
+% use the scores in order to get the 8 points instead of random sampling
+matches = matches(:, randi(size(matches,2),1,8));
+size(matches)
+% matches = matches()
+% 
+% n = size(matches, 2); % ::= 8
+% 
+% % get A matrix
+% A = getA(i_points1, i_points2, matches);
 
-% sample 8 points from matches
-rand8   = rand()
-matches = matches()
-
-n = size(matches, 2); % ::= 8
-
-% get A matrix
-A = getA(i_points1, i_points2, matches);
 
 
-
-%% plot transformation
+%% plot matches
 concat = cat(2, i1, i2); concat = concat / 255;
 figure;  imshow(concat, 'InitialMagnification', 50);
 hold on
@@ -37,9 +44,8 @@ destinationPoints(:, 1) = destinationPoints(:, 1) + size(i1,2); % plus width
 
 plot(originPoints(1:end, 1), originPoints(1:end, 2), 'ro');
 plot(destinationPoints(1:end, 1), destinationPoints(1:end, 2), 'go');
-% maybe without loop?
-% only every 20th line, otherwise it's a mess
-for i=1:25:length(destinationPoints),
+
+for i=1:length(destinationPoints),
   plot([originPoints(i, 1) destinationPoints(i, 1)], [originPoints(i, 2) destinationPoints(i, 2) ], 'b');
 end
 hold off
--- a2/getForegroundSift.m
+++ a2/getForegroundSift.m
@@ -1,9 +1,10 @@
-function [ i_points, desc ] = getForegroundSift( i_points, desc )
+function [ foreground_points, foreground_desc ] = getForegroundSift( i_points, desc )
 %GETSIFTSAMPLE Returns a subset of the interest points eliminating
 %background points
-
-    dsearchn()
-
-
+    centerOfMass = [ mean(i_points(1,:)); mean(i_points(2,:)) ];
+    temp = i_points(1:2,:)';
+    tree = kdtree_build( temp );
+    IDX = kdtree_k_nearest_neighbors(tree, centerOfMass', 2000);
+    foreground_points = i_points(:, IDX);
+    foreground_desc = desc(:,IDX);
 end
-
