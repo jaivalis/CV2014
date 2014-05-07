418730b3dd097faa63f2a024d16210782860850c
TopSteely <tobias.stahl1@gmx.de>
2014-05-07 19:27:38 +0200

    wokring on chaining!

--- a2/A2.m
+++ a2/A2.m
@@ -1,6 +1,7 @@
 clear all
 addpath('../a1/kdtree')
-pointView = zeros(0);
+pointView  = []; % images x points
+pointsSeen = []; % points x 2 (xy)
 for i = 1 : 1%16
     i1 = single(rgb2gray(imread(strcat('TeddyBear/obj02_00', num2str(i),'.png'))));
     if i < 16
@@ -49,5 +50,25 @@ for i = 1 : 1%16
 
     %% Chaining
 
-    pointView = cat(2,pointView, F);
+    % Start from any two consecutive image matches. Add a new column to
+    % point-view matrix for each newly introduced point.
+    % If a point which is already introduced in the point-view matrix and an-
+    % other image contains that point, mark this matching on your point-view
+    % matrix using the previously defined point column. Do not introduce a new
+    % column.
+
+    
+    for k = 1:size(matches, 2)
+        if ~sum(ismember(pointsSeen', [i_points2(1, matches(2,k)) i_points2(2, matches(2,k))]', 'rows'))
+            % add a new column for any newly introduced point
+            pointsSeen = cat(2, pointsSeen, i_points2(1, matches(2,k)));
+            pointView  = cat(2, pointView, zeros(16, 1));
+        else 
+            % mark the point of points that have already been seen
+            % find point in pointsSeen
+            [c,r] = find(pointsSeen == [i_points2(1, matches(2,k)) i_points2(2, matches(2,k))]);
+            % mark point in pointView
+            pointView(c, i) = 1;
+        end
+    end
 end
