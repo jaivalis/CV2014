a21f00e12a09c713a798ad205c97e0eb5b3d8bfd
jaivalis <johnaivalis@gmail.com>
2014-05-19 14:16:55 +0200

    fixed var ICP names

--- a1/ICP.m
+++ a1/ICP.m
@@ -1,31 +1,33 @@
+clear all;
 addpath('kdtree')
 
 % params
 neighbors  = 1;
 type       = 'random';
 sampleSize = 10000;
+stepSize   = 10;
 % \params
 
 base   = getPcd( 0, type, sampleSize );
-for image = 1:99
+for image = 1:stepSize:99
     disp(strcat('Image: ', num2str(image)))
     
     target = getPcd( image, type, sampleSize );
 
     %% find closest points
-    tree = kdtree_build( base_coords );
+    tree = kdtree_build( base );
     % input_data: d x n-matrix containing n d-dimensional points,
-    idxs = zeros(size(target_coords, 1), neighbors);
+    idxs = zeros(size(target, 1), neighbors);
 
     % calculate new RMS
-    [RMS, idxs] = calculateRMS(tree, idxs, base_coords, target_coords, neighbors);
+    [RMS, idxs] = calculateRMS(tree, idxs, base, target, neighbors);
 
     %% Phase 2: Finding the geometric centroid of accepted matches
-    size_base   = size(base_coords,1);
-    mu_base     = [sum(base_coords(:,1)) sum(base_coords(:,2)) sum(base_coords(:,3))] / size_base;
+    size_base   = size(base,1);
+    mu_base     = [sum(base(:,1)) sum(base(:,2)) sum(base(:,3))] / size_base;
 
     % Shift the center of mass of the base point cloud to the origin of the coordinate system.
-    basePrime   = [ base_coords(idxs,1) - mu_base(1) base_coords(idxs,2) - mu_base(2) base_coords(idxs,3) - mu_base(3) ];
+    basePrime   = [ base(idxs,1) - mu_base(1) base(idxs,2) - mu_base(2) base(idxs,3) - mu_base(3) ];
     
     iteration   = 1;
     while 1
@@ -36,9 +38,9 @@ for image = 1:99
         % calculate A matrix
 
         % Shift the center of mass of the target point cloud to the origin of the coordinate system.
-        size_target = size(target_coords,1);
-        mu_target   = [sum(target_coords(:,1)) sum(target_coords(:,2)) sum(target_coords(:,3)) ] / size_target;
-        targetPrime = [ target_coords(:,1) - mu_target(1) target_coords(:,2) - mu_target(2) target_coords(:,3) - mu_target(3) ];
+        size_target = size(target,1);
+        mu_target   = [sum(target(:,1)) sum(target(:,2)) sum(target(:,3)) ] / size_target;
+        targetPrime = [ target(:,1) - mu_target(1) target(:,2) - mu_target(2) target(:,3) - mu_target(3) ];
 
         A = basePrime' * targetPrime;
 
@@ -58,19 +60,19 @@ for image = 1:99
         T   = B_c - T_c * R;
 
         %% Phase 5: Calculate new average distances using the rotation and translation matrix
-        temp = R * target_coords';
+        temp = R * target';
 
-        target_coords = (R * target_coords')';
-        target_coords = [target_coords(:, 1) + T(1) ...
-                         target_coords(:, 2) + T(2) ...
-                         target_coords(:, 3) + T(3)];
+        target = (R * target')';
+        target = [target(:, 1) + T(1) ...
+                         target(:, 2) + T(2) ...
+                         target(:, 3) + T(3)];
 
         % calculate new RMS
-        [RMS, idxs] = calculateRMS(tree, idxs, base_coords, target_coords, neighbors);  
+        [RMS, idxs] = calculateRMS(tree, idxs, base, target, neighbors);  
 
         iteration = iteration + 1;
 
-        disp(strcat('RMS:', num2str(RMS), ' prevRMS:', num2str(prevRMS),...
+        disp(strcat('RMS:', num2str(RMS), ' prevRMS:', num2str(prev_RMS),...
                     ' Difference:', num2str(abs(prev_RMS - RMS))))
 
         % Loop until RMS remains unchanged
@@ -86,12 +88,12 @@ for image = 1:99
 %     subplot(1,2,2);
 %     scatter3(target_coords(1:100:end,1),target_coords(1:100:end,2),target_coords(1:100:end,3),'b')
     figure;
-    scatter3(base_coords(1:100:end,1),base_coords(1:100:end,2),base_coords(1:100:end,3),'r')
+    scatter3(base(1:100:end,1),base(1:100:end,2),base(1:100:end,3),'r')
     hold on;
-    scatter3(target_coords(1:100:end,1),target_coords(1:100:end,2),target_coords(1:100:end,3),'b')
+    scatter3(target(1:100:end,1),target(1:100:end,2),target(1:100:end,3),'b')
     hold off;
     
     % merge base and target
-    base_coords = merge(base_coords, target_coords, idxs);
+    base = merge(base, target, idxs);
     
 end
